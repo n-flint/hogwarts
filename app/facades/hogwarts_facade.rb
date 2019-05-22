f952040c1dc9cbd7674e6f08e66cb04b7bebf4e7
Noah Flint <noah.flint24@gmail.com>
2019-05-22 14:26:39 -0600

    made api call for students

--- app/facades/hogwarts_facade.rb
+++ app/facades/hogwarts_facade.rb
@@ -1,11 +1,16 @@
 class HogwartsFacade
 
   def initialize(house)
-    @house = house
+    @house = house.downcase
   end
 
   def student_count
-
+    conn = Faraday.new('http://hp-api.herokuapp.com/api/characters/house/') do |f|
+      f.adapter Faraday.default_adapter
+    end
+    response = conn.get(@house)
+    data = JSON.parse(response.body, symbolize_name: true)
+    data.count 
   end
 
 end
