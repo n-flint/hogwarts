ffdde8bab3fdda44baa091745cf141d262a0a2a7
Noah Flint <noah.flint24@gmail.com>
2019-05-22 15:53:55 -0600

    changed api call (was using the wrong one)

--- app/facades/hogwarts_facade.rb
+++ app/facades/hogwarts_facade.rb
@@ -4,28 +4,31 @@ class HogwartsFacade
     @house = house
   end
 
-  def student_count
-    conn = Faraday.new('http://hp-api.herokuapp.com/api/characters/house/') do |f|
-      f.adapter Faraday.default_adapter
-    end
-    response = conn.get(@house)
-    data = JSON.parse(response.body, symbolize_name: true)
-    data.count 
-  end
+  # def student_count
+  #   conn = Faraday.new('http://hp-api.herokuapp.com/api/characters/house/') do |f|
+  #     f.adapter Faraday.default_adapter
+  #   end
+  #   response = conn.get(@house)
+  #   data = JSON.parse(response.body, symbolize_name: true)
+  #   data.count 
+  #   # require 'pry'; binding.pry
+  # end
 
   def students
-    conn = Faraday.new('http://hp-api.herokuapp.com/api/characters/house/') do |f|
+    conn = Faraday.new("http://hogwarts-it.herokuapp.com/api/v1/house/#{@house}?api_key=alohamora") do |f|
       f.adapter Faraday.default_adapter
     end
-    response = conn.get(@house)
+    response = conn.get
     data = JSON.parse(response.body, symbolize_name: true)
     students = []
+    data[:data]
     data.each do |char|
+      require 'pry'; binding.pry
       if char['hogwartsStudent']
         students << Student.new(char['name'])
       end
     end
     students
-    # require 'pry'; binding.pry
+    require 'pry'; binding.pry
   end
 end
