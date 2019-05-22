151e43e98dea7f626a824910781615286b7f0576
Noah Flint <noah.flint24@gmail.com>
2019-05-22 14:57:36 -0600

    added students method

--- app/facades/hogwarts_facade.rb
+++ app/facades/hogwarts_facade.rb
@@ -1,7 +1,7 @@
 class HogwartsFacade
 
   def initialize(house)
-    @house = house.downcase
+    @house = house
   end
 
   def student_count
@@ -13,4 +13,19 @@ class HogwartsFacade
     data.count 
   end
 
+  def students
+    conn = Faraday.new('http://hp-api.herokuapp.com/api/characters/house/') do |f|
+      f.adapter Faraday.default_adapter
+    end
+    response = conn.get(@house)
+    data = JSON.parse(response.body, symbolize_name: true)
+    students = []
+    data.each do |char|
+      if char['hogwartsStudent']
+        students << Student.new(char['name'])
+      end
+    end
+    students
+    # require 'pry'; binding.pry
+  end
 end
